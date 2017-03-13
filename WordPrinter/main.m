7038c4bcc74e74325abd76e387514f9e570c7bf9
neliSun <nelson.chow91.nc@gmail.com>
2017-03-13 14:33:12 -0700

    Initial Commit

---
+++ WordPrinter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordPrinter
+//
+//  Created by Nelson Chow on 2017-03-13.
+//  Copyright © 2017 Nelson Chow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
